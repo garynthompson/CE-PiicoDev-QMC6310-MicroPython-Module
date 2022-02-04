d72e9db49bfe72826b9e87056962f38a9250ba9d
Michael Ruppe <73618042+CoreProduction@users.noreply.github.com>
2022-02-04 16:48:22 +1100

    Update compass.py

--- example/compass.py
+++ example/compass.py
@@ -1,5 +1,8 @@
-# compass
-from PiicoDev_QMC6310 import *
+# Calibrate the sensor by turning it in a circle
+# Display the heading
+
+from PiicoDev_QMC6310 import PiicoDev_QMC6310
+from PiicoDev_Unified import sleep_ms
 
 magnetometer = PiicoDev_QMC6310()
 
@@ -8,4 +11,4 @@ magnetometer.calibrate()
 while True:
     heading = magnetometer.readHeading()
     print(heading)
-    sleep_ms(1000)
+    sleep_ms(1000)    sleep_ms(100)
