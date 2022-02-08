439bc1129070a3ce42d5a6912ea3c17053d2526c
Michael Ruppe <73618042+CoreProduction@users.noreply.github.com>
2022-02-08 16:01:40 +1100

    add comments

--- example/rawData.py
+++ example/rawData.py
@@ -3,9 +3,9 @@
 from PiicoDev_QMC6310 import PiicoDev_QMC6310
 from PiicoDev_Unified import sleep_ms
 
-magSensor = PiicoDev_QMC6310()
+magSensor = PiicoDev_QMC6310() # Initialise the sensor
 
 while True:
-    raw_data = magSensor.read()
-    print(raw_data)
+    raw_data = magSensor.read() # Read the field strength on each axis
+    print(raw_data)             # Print the data
     sleep_ms(200)
