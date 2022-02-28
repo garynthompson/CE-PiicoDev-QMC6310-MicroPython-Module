28df73794f230f9daaf420e21e05a87e75687052
Michael Ruppe <73618042+CoreProduction@users.noreply.github.com>
2022-03-01 10:37:25 +1100

    update initial range 800uT

--- example/compass.py
+++ example/compass.py
@@ -7,7 +7,7 @@ from PiicoDev_Unified import sleep_ms          # Cross-platform compatible sleep
 compass = PiicoDev_QMC6310(range=800)          # Initialise the sensor with 800uT range. Valid ranges: 200, 800, 1200, 3000 uT
 
 # Calibration recommended for best results.
-# compass.calibrate() # only need to calibrate once
+compass.calibrate() # only need to calibrate once
 
 # Optional: Declination is the difference between magnetic-north and true-north ("heading") and depends on location
 # compass.setDeclination(12.3) # Found with: https://www.magnetic-declination.com/Australia/Newcastle/122944.html
