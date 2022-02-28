df440db3487237df6e1bf26c5451aed903cd8421
GitHub Action <action@github.com>
2022-02-28 23:40:08 +0000

    Update min/PiicoDev_QMC6310.py

--- min/PiicoDev_QMC6310.py
+++ min/PiicoDev_QMC6310.py
@@ -1,6 +1,6 @@
-_E=False
+_E=True
 _D=None
-_C='z'
+_C=False
 _B='y'
 _A='x'
 import math
@@ -35,7 +35,7 @@ class PiicoDev_QMC6310:
 		self.i2c=create_unified_i2c(bus=bus,freq=freq,sda=sda,scl=scl);self.addr=addr;self.odr=odr;self.calibrationFile=calibrationFile;self._CR1=0;self._CR2=0
 		try:self._setMode(1);self.setOutputDataRate(odr);self.setOverSamplingRatio(osr1);self.setOverSamplingRate(osr2);self.setRange(range)
 		except Exception as e:print(i2c_err_str.format(self.addr));raise e
-		self.x_offset=0;self.y_offset=0;self.z_offset=0;self.declination=0;self.data={};self._dataValid=_E;self.loadCalibration()
+		self.x_offset=0;self.y_offset=0;self.z_offset=0;self.declination=0;self.data={};self._dataValid=_C;self.loadCalibration()
 	def _setMode(self,mode):self._CR1=_writeCrumb(self._CR1,_BIT_MODE,mode);self.i2c.writeto_mem(self.addr,_ADDRESS_CONTROL1,bytes([self._CR1]))
 	def setOutputDataRate(self,odr):self._CR1=_writeCrumb(self._CR1,_BIT_ODR,odr);self.i2c.writeto_mem(self.addr,_ADDRESS_CONTROL1,bytes([self._CR1]))
 	def setOverSamplingRatio(self,osr1):self._CR1=_writeCrumb(self._CR1,_BIT_OSR1,osr1);self.i2c.writeto_mem(self.addr,_ADDRESS_CONTROL1,bytes([self._CR1]))
@@ -48,49 +48,42 @@ class PiicoDev_QMC6310:
 	def _getControlRegisters(self):return self.i2c.readfrom_mem(self.addr,_ADDRESS_CONTROL1,2)
 	def _getStatusReady(self,status):return _readBit(status,0)
 	def _getStatusOverflow(self,status):return _readBit(status,1)
-	def read(self):
-		C='little';B=True;A='NaN';self._dataValid=_E;NaN={_A:float(A),_B:float(A),_C:float(A)}
+	def read(self,raw=_C):
+		B='little';A='NaN';self._dataValid=_C;NaN={_A:float(A),_B:float(A),'z':float(A)}
 		try:status=int.from_bytes(self.i2c.readfrom_mem(self.addr,_ADDRESS_STATUS,1),'')
 		except:print(i2c_err_str.format(self.addr));self.sample=NaN;return NaN
-		if self._getStatusReady(status)is B:
-			try:x=int.from_bytes(self.i2c.readfrom_mem(self.addr,_ADDRESS_XOUT,2),C);y=int.from_bytes(self.i2c.readfrom_mem(self.addr,_ADDRESS_YOUT,2),C);z=int.from_bytes(self.i2c.readfrom_mem(self.addr,_ADDRESS_ZOUT,2),C)
+		if self._getStatusReady(status)is _E:
+			try:x=int.from_bytes(self.i2c.readfrom_mem(self.addr,_ADDRESS_XOUT,2),B);y=int.from_bytes(self.i2c.readfrom_mem(self.addr,_ADDRESS_YOUT,2),B);z=int.from_bytes(self.i2c.readfrom_mem(self.addr,_ADDRESS_ZOUT,2),B)
 			except:print(i2c_err_str.format(self.addr));self.sample=NaN;return self.sample
-			if self._getStatusOverflow(status)is B:return NaN
+			if self._getStatusOverflow(status)is _E:return NaN
 			if x>=32768:x=-(65535-x+1)
-			x=(x-self.x_offset)*self.sensitivity
+			x=x-self.x_offset
 			if y>=32768:y=-(65535-y+1)
-			y=(y-self.y_offset)*self.sensitivity
+			y=y-self.y_offset
 			if z>=32768:z=-(65535-z+1)
-			z=(z-self.z_offset)*self.sensitivity;self.sample={_A:x,_B:y,_C:z};self._dataValid=B;return self.sample
+			z=z-self.z_offset
+			if raw is _C:x*=self.sensitivity;y*=self.sensitivity;z*=self.sensitivity
+			self.sample={_A:x,_B:y,'z':z};self._dataValid=_E;return self.sample
 		else:print('Not Ready');self.sample=NaN;return self.sample
 	def dataValid(self):return self._dataValid
-	def readPolar(self):cartesian=self.read();pi=math.pi;angle=math.atan2(cartesian[_A],cartesian[_B])/pi*180.0+self.declination;angle=self._convertAngleToPositive(angle);magnitude=math.sqrt(cartesian[_A]*cartesian[_A]+cartesian[_B]*cartesian[_B]+cartesian[_C]*cartesian[_C]);return{'polar':angle,'Gauss':magnitude*100,'uT':magnitude}
+	def readPolar(self):cartesian=self.read();angle=math.atan2(cartesian[_A],cartesian[_B])/math.pi*180.0+self.declination;angle=self._convertAngleToPositive(angle);magnitude=math.sqrt(cartesian[_A]*cartesian[_A]+cartesian[_B]*cartesian[_B]+cartesian['z']*cartesian['z']);return{'polar':angle,'Gauss':magnitude*100,'uT':magnitude}
 	def readMagnitude(self):return self.readPolar()['uT']
 	def readHeading(self):return self.readPolar()['polar']
 	def setDeclination(self,dec):self.declination=dec
-	def calibrate(self,enable_logging=_E):
+	def calibrate(self,enable_logging=_C):
 		try:self.setOutputDataRate(3)
 		except Exception as e:print(i2c_err_str.format(self.addr));raise e
-		x_min=0;x_max=0;y_min=0;y_max=0;z_min=0;z_max=0;log='';print('*** Calibrating.\n    Slowly rotate your sensor until the bar is full');print('[          ]',end='');range=1000;i=0
+		x_min=65535;x_max=-65535;y_min=65535;y_max=-65535;z_min=65535;z_max=-65535;log='';print('*** Calibrating.\n    Slowly rotate your sensor until the bar is full');print('[          ]',end='');range=1000;i=0;x=0;y=0;z=0;a=0.5
 		while i<range:
-			i+=1;sleep_ms(5);cartesian=self.read()
-			if cartesian[_A]<x_min:x_min=cartesian[_A];i=0
-			if cartesian[_A]>x_max:x_max=cartesian[_A];i=0
-			if cartesian[_B]<y_min:y_min=cartesian[_B];i=0
-			if cartesian[_B]>y_max:y_max=cartesian[_B];i=0
-			if cartesian[_C]<z_min:z_min=cartesian[_C];i=0
-			if cartesian[_C]>z_max:z_max=cartesian[_C];i=0
-			if i==range/10:print('\r[*         ]',end='')
-			if i==2*range/10:print('\r[**        ]',end='')
-			if i==3*range/10:print('\r[***       ]',end='')
-			if i==4*range/10:print('\r[****      ]',end='')
-			if i==5*range/10:print('\r[*****     ]',end='')
-			if i==6*range/10:print('\r[******    ]',end='')
-			if i==7*range/10:print('\r[*******   ]',end='')
-			if i==8*range/10:print('\r[********  ]',end='')
-			if i==9*range/10:print('\r[********* ]',end='')
-			if i==10*range/10-1:print('\r[**********]')
-			if enable_logging:log=log+(str(cartesian[_A])+','+str(cartesian[_B])+','+str(cartesian[_C])+'\n')
+			i+=1;sleep_ms(5);d=self.read(raw=_E);x=a*d[_A]+(1-a)*x;y=a*d[_B]+(1-a)*y;z=a*d['z']+(1-a)*z
+			if x<x_min:x_min=x;i=0
+			if x>x_max:x_max=x;i=0
+			if y<y_min:y_min=y;i=0
+			if y>y_max:y_max=y;i=0
+			if z<z_min:z_min=z;i=0
+			if z>z_max:z_max=z;i=0
+			j=round(10*i/range);print('\r['+int(j)*'*'+int(10-j)*' '+']',end='')
+			if enable_logging:log=log+(str(cartesian[_A])+','+str(cartesian[_B])+','+str(cartesian['z'])+'\n')
 		self.setOutputDataRate(self.odr);self.x_offset=(x_max+x_min)/2;self.y_offset=(y_max+y_min)/2;self.z_offset=(z_max+z_min)/2;f=open(self.calibrationFile,'w');f.write('x_min:\n'+str(x_min)+'\nx_max:\n'+str(x_max)+'\ny_min:\n'+str(y_min)+'\ny_max:\n'+str(y_max)+'\nz_min\n'+str(z_min)+'\nz_max:\n'+str(z_max)+'\nx_offset:\n');f.write(str(self.x_offset)+'\ny_offset:\n'+str(self.y_offset)+'\nz_offset:\n'+str(self.z_offset));f.close()
 		if enable_logging:flog=open('calibration.log','w');flog.write(log);flog.close
 	def loadCalibration(self):
